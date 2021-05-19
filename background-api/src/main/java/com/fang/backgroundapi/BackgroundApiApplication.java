1e99f6db9355418094508763164779acfe82a386
cxqweqwe <2394197228@qq.com>
2021-05-19 17:15:59 +0800

    初始化项目

---
+++ background-api/src/main/java/com/fang/backgroundapi/BackgroundApiApplication.java
@@ -0,0 +1,13 @@
+package com.fang.backgroundapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackgroundApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackgroundApiApplication.class, args);
+    }
+
+}
---
+++ background-api/src/test/java/com/fang/backgroundapi/BackgroundApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fang.backgroundapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackgroundApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
