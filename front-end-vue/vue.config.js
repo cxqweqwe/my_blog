b08c6db2ca8aa37d7734bfe7d7b215c3503772e6
cxqweqwe <2394197228@qq.com>
2022-01-04 21:35:40 +0800

    更改配置

--- front-end-vue/vue.config.js
+++ front-end-vue/vue.config.js
@@ -1,29 +1,35 @@
 module.exports = {
-	lintOnSave: false,
-	configureWebpack: {
-		resolve: {
-			alias: {
-				'assets': '@/assets',
-				'common': '@/common',
-				'components': '@/components',
-				'network': '@/network',
-				'views': '@/views',
-				'router': '@/router',
-				'store': '@/store'
-			}
-		},
-	},
-	chainWebpack: config => {
-		config.module
-			.rule('md')
-			.test(/\.md$/)
-			.use('html-loader')
-			.loader('html-loader')
-			.end()
-			.use('markdown-loader')
-			.loader('markdown-loader')
-			.end()
-	}
+  lintOnSave: false,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views',
+        'router': '@/router',
+        'store': '@/store'
+      }
+    },
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('md')
+      .test(/\.md$/)
+      .use('html-loader')
+      .loader('html-loader')
+      .end()
+      .use('markdown-loader')
+      .loader('markdown-loader')
+      .end()
+  },
+  devServer: {
+    port: 8080,   //默认是8080
+    open: false,  //取消启动自动打开页面
+    hot: true,    //自动刷新页面
+    // host : "0.0.0.0"
+  },
 };
 
 
