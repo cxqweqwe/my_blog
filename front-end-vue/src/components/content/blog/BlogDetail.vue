5e0d3e0ef7b9a546921f94f939b94e5a39c295d3
cxqweqwe <2394197228@qq.com>
2022-01-25 18:23:58 +0800

    评论功能静态完成

--- front-end-vue/src/components/content/blog/BlogDetail.vue
+++ front-end-vue/src/components/content/blog/BlogDetail.vue
@@ -1,52 +1,58 @@
 <template>
-  <div class="blog blogDetail">
-    <div class="info">
-      <Row>
-        <Col span="24">
-          <h1>{{ article.title }}</h1>
-        </Col>
-      </Row>
-      <div class="space"></div>
-      <Row>
-        <Col span="16">
-          <Avatar style="color: #ffffff;background-color: #ea7f7f">U</Avatar>&nbsp;
-          <div class="nick-name">
-            {{ nickName }}
-          </div>
-        </Col>
-        <Col span="8">
-          <div class="time">
-            <span class="time-text">发布时间&nbsp; {{ article.modifiedTime }}</span>
-          </div>
-        </Col>
-      </Row>
-      <div class="space"></div>
-      <Row>
-        <Col span="16">
-          标签:
-          <Tag color="volcano" v-for="(item,index) in labels" :key="index" v-text="item"></Tag>
-        </Col>
-        <Col span="5">
-          <div class="view-collect">
-            <i class="el-icon-view"></i>&nbsp;{{ pageviews }}
-            <Icon type="md-star"/>
-            {{ blogCollection }}
-          </div>
-        </Col>
-        <Col span="3">
-          <div class="like" @click="likeClick">
-            <Icon type="ios-thumbs-up-outline" v-if="!likeOrNot" title="喜欢就点个赞吧~"/>
-            <Icon type="ios-thumbs-up" v-else title="已喜欢"/>
-            {{ likes }}
-          </div>
-        </Col>
-      </Row>
+  <div>
+    <div class="blog blogDetail">
+      <div class="info">
+        <Row>
+          <Col span="24">
+            <h1>{{ article.title }}</h1>
+          </Col>
+        </Row>
+        <div class="space"></div>
+        <Row>
+          <Col span="16">
+            <Avatar style="color: #ffffff;background-color: #ea7f7f">U</Avatar>&nbsp;
+            <div class="nick-name">
+              {{ nickName }}
+            </div>
+          </Col>
+          <Col span="8">
+            <div class="time">
+              <span class="time-text">发布时间&nbsp; {{ article.modifiedTime }}</span>
+            </div>
+          </Col>
+        </Row>
+        <div class="space"></div>
+        <Row>
+          <Col span="16">
+            标签:
+            <Tag color="volcano" v-for="(item,index) in labels" :key="index" v-text="item"></Tag>
+          </Col>
+          <Col span="5">
+            <div class="view-collect">
+              <i class="el-icon-view"></i>&nbsp;{{ pageviews }}
+              <Icon type="md-star"/>
+              {{ blogCollection }}
+            </div>
+          </Col>
+          <Col span="3">
+            <div class="like" @click="likeClick">
+              <Icon type="ios-thumbs-up-outline" v-if="!likeOrNot" title="喜欢就点个赞吧~"/>
+              <Icon type="ios-thumbs-up" v-else title="已喜欢"/>
+              {{ likes }}
+            </div>
+          </Col>
+        </Row>
+      </div>
+      <div class="blog-body">
+        <div v-html="article.html" class="markdown-body" v-highlight></div>
+      </div>
     </div>
-    <div class="blog-body">
-      <div v-html="article.html" class="markdown-body" v-highlight></div>
+    <div class="space"></div>
+    <div class="common-div">
+      <Comment></Comment>
     </div>
+    <div class="space"></div>
   </div>
-
 </template>
 
 <script>
@@ -55,8 +61,14 @@
   import {getAuthorInfo} from "network/userInfo";
   import {getBlogInfo, likeBlog, unLikeBlog} from "network/blogInfo";
 
+  import Comment from "components/content/comment/Comment";
+
+
   export default {
     name: "BlogDetail",
+    components: {
+      Comment,
+    },
     data() {
       return {
         // md: demo,
@@ -81,7 +93,7 @@
         blogViews: 0,
         pageviews: 0,
         blogCollection: 0,
-        likes: 0
+        likes: 0,
       }
     },
     created() {
@@ -176,9 +188,10 @@
           this.likes = res.data.likes;
           this.pageviews = res.data.pageviews;
         })
-      }
+      },
 
 
+      // 方法结束
     }
 
   }
@@ -280,5 +293,13 @@
     .markdown-body {
       padding: 15px;
     }
+  }
+
+  /* 评论部分 */
+  .common-div{
+    padding: 10px;
+    border: 1px solid #ebebeb;
+    border-radius: 15px;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
   }
 </style>
